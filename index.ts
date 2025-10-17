e76ce3f401bbc8bda55ff5e6d9a6da1831f7ab7d
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2025-10-17 10:10:58 +0200

    fix: url generation

--- index.ts
+++ index.ts
@@ -2,46 +2,20 @@
 import dailyStateFromUrl from './lib/daily-state.js'
 import syncWithOld from './lib/sync-with-old.js'
 import diffBulk from './lib/diff-bulk.js'
-import fs from 'fs-extra'
-import path from 'path'
-import datasetSchema from './resources/schema.json' with { type: 'json' }
 
-console.log(datasetSchema)
-
-export const run = async ({ processingConfig, processingId, dir, axios, log, patchConfig }) => {
-  let dataset
-  if (processingConfig.datasetMode === 'create') {
-    await log.step('Création du jeu de données')
-    dataset = (await axios.post('api/v1/datasets', {
-      id: processingConfig.dataset.id,
-      title: processingConfig.dataset.title,
-      isRest: true,
-      schema: datasetSchema,
-      primaryKey: ['siret', 'code_qualification', 'date_debut'],
-      extras: { processingId }
-    })).data
-    await log.info(`jeu de donnée créé, id="${dataset.id}", title="${dataset.title}"`)
-    await patchConfig({ datasetMode: 'update', dataset: { id: dataset.id, title: dataset.title } })
-  } else if (processingConfig.datasetMode === 'update') {
-    await log.step('Vérification du jeu de données')
-    dataset = (await axios.get(`api/v1/datasets/${processingConfig.dataset.id}`)).data
-    if (!dataset) throw new Error(`le jeu de données n'existe pas, id${processingConfig.dataset.id}`)
-    await log.info(`le jeu de donnée existe, id="${dataset.id}", title="${dataset.title}"`)
-  }
-
+export const run = async ({ processingConfig, axios, log }) => {
   for (const organism of processingConfig.organisms) {
     log.step(`Traitement du répertoire ${organism}`)
-    await fs.ensureDir(path.join(dir, organism))
     if (processingConfig.syncWithOld) await syncWithOld(processingConfig, organism, axios, log)
 
     if (processingConfig.updateFromDaily) {
-      const previousState = await dailyStateFromUrl(processingConfig.currentHistoryDataset, organism, true, axios, log)
-      const currentState = await dailyStateFromUrl(processingConfig.dailyDataset, organism, false, axios, log)
+      const previousState = await dailyStateFromUrl(`api/v1/datasets/${processingConfig.currentHistoryDataset.id}`, organism, true, axios, log)
+      const currentState = await dailyStateFromUrl(`api/v1/datasets/${processingConfig.dailyDataset.id}`, organism, false, axios, log)
       const { stats, bulk } = await diffBulk(previousState, currentState, processingConfig)
       await log.info(`enregistrement des modifications : ouvertures=${stats.created}, fermetures=${stats.closed}, modifications=${stats.updated}, inchangés=${stats.unmodified}`)
       while (bulk.length) {
         const lines = bulk.splice(0, 1000)
-        const res = await axios.post(`api/v1/datasets/${dataset.id}/_bulk_lines`, lines)
+        const res = await axios.post(`api/v1/datasets/${processingConfig.currentHistoryDataset.id}/_bulk_lines`, lines)
         if (res.data.nbErrors) log.error(`${res.data.nbErrors} échecs sur ${lines.length} lignes à insérer`, res.data.errors)
       }
     }
---
+++ test/test.ts
@@ -0,0 +1,42 @@
+import * as assert from 'assert'
+import pluginConfigSchema from '../plugin-config-schema.json' with { type: 'json' }
+import processingConfigSchema from '../processing-config-schema.json' with { type: 'json' }
+import testsUtils from '@data-fair/lib-processing-dev/tests-utils.js'
+import config from 'config'
+import * as rgePlugin from '../index.ts'
+import { it, describe } from 'node:test'
+
+process.env.NODE_ENV = 'test'
+
+describe('Hello world processing', () => {
+  it('should expose a plugin config schema for super admins', async () => {
+    assert.strict.ok(pluginConfigSchema)
+  })
+
+  it('should expose a processing config schema for users', async () => {
+    assert.strict.ok(processingConfigSchema)
+  })
+
+  it('should run a task', async function () {
+    const context = testsUtils.context({
+      pluginConfig: {},
+      processingConfig: {
+        organisms: [
+          'qualifelec'
+        ],
+        syncWithOld: true,
+        updateFromDaily: false,
+        forceLinkDatesUpdate: false,
+        oldHistoryDataset: {
+          title: 'Historique RGE - old',
+          id: 'pusi0muwdfrxq4a6ygwrz1b6'
+        },
+        currentHistoryDataset: {
+          title: 'Historique RGE - new',
+          id: '9nj6ed-4re2j95njni3qkne7'
+        }
+      },
+    }, config, false, false)
+    await rgePlugin.run(context)
+  })
+})
